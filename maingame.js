f9fb611e1bd5579f1f4e8df6d0283aabd166383e
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-02-05 19:11:56 +0100

    Added Files

---
+++ maingame.js
@@ -0,0 +1,63 @@
+let difficulty = 1
+
+let score = 0
+let gameOver = false
+
+let images = []
+
+function preload() {
+  images.coin = loadImage("resources/coin.png")
+  images.asteroid = loadImage("resources/asteroid.png")
+  images.background = loadImage("resources/space.png")
+  images.spaceship = loadImage("resources/spaceship.png")
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  // Opgave 3.1.4 - background
+  imageMode(LEFT)
+  image(images.background, 0, 0, windowWidth, windowHeight)
+  
+  // Opgave 3.5.2 "is game over?"
+  //if( ... ) {
+    
+    // ...
+    // ...
+    
+    tickObjects()
+    tickSpawning()
+    tickScore()
+    tickDifficulty()
+  //}
+  
+  //Opgave 3.4.4 drawUI
+  // ...
+  // ...
+}
+
+  
+function spliceAll(array, splices) {
+}
+  
+function tickScore() {
+}
+
+function tickDifficulty() {
+}
+
+function drawUI() {
+}
+
+function tickObjects() {
+
+}
+
+function spawnNewObject(type) {
+}
+
+function tickSpawning() {
+}
